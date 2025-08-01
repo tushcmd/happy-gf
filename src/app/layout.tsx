a5ffbd399bd64e892d5461a03a985649f359339d
tushcmd <muturidavid854@gmail.com>
2025-08-01 15:34:35 +0300

    analytics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId='G-SBCYMJB7H3' />
     </html>
   )
 }
